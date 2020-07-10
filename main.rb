b56023b0dce7490ecc2d798fd860e4ccd22a771d
Ahmad Chata <ahmadchata@gmail.com>
2020-07-10 16:36:42 +0100

    Add my_all method

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
+
     array = to_a
     i = 0
     while i < array.length
@@ -12,6 +13,7 @@ module Enumerable
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
+
     array = to_a
     i = 0
     while i < array.length
@@ -19,14 +21,20 @@ module Enumerable
       i += 1
     end
     array
-   end
+  end
 
   def my_select
     return to_enum(:my_select) unless block_given?
+
     new_array = []
-    my_each{| elem | new_array.push(elem) if yield(elem) == true}
+    my_each { |elem| new_array.push(elem) if yield(elem) == true }
     new_array
-   end
+  end
+
+  def my_all?
+    my_each { |elem| return false if yield(elem) == false }
+    true
+  end
 end
 
 # fruits = w%[apple banana strawberry pineapple]
@@ -37,5 +45,5 @@ end
 friends = %w[Sharon Leo Leila Brian Arun]
 
 # x = friends.my_each { |friend| friend.upcase }
-x=friends.my_select { |friend| friend == 'Brian' }
+x = friends.my_all? { |friend| friend.length >= 4 }
 p x
