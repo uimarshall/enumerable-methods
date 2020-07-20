18e5eb476d192de3eed3f05fc85a83f8b9d6f60b
Ahmad Chata <ahmadchata@gmail.com>
2020-07-20 15:51:27 +0100

    Update my_none? method and fix rubocop errors

--- main.rb
+++ main.rb
@@ -4,7 +4,6 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    array = to_a
     array = [Hash, Range].member?(self.class) ? to_a.flatten : self
     i = 0
     while i < array.length
@@ -35,7 +34,6 @@ module Enumerable
   end
 
   def my_all?(arg = nil)
-
     if block_given?
       my_each { |elem| return false unless yield(elem) }
     elsif arg.nil?
@@ -50,36 +48,31 @@ module Enumerable
     true
   end
 
-  def my_any?(*arg)
-    if arg.empty?
-      if block_given?
-        my_each { |elem| return true if yield(elem) }
-      else
-        my_each { |elem| return true unless elem.nil? || elem == false }
-      end
-
+  def my_any?(arg = nil)
+    if block_given?
+      my_each { |elem| return true if yield(elem) }
+    elsif arg.nil?
+      my_each { |elem| return true unless elem.nil? || elem == false }
     elsif arg[0].is_a?(Class)
       my_each { |elem| return true if elem.is_a?(arg[0]) }
     elsif arg[0].is_a?(Regexp)
       my_each { |elem| return true if elem.match?(arg[0]) }
+    elsif arg
+      my_each { |elem| return true if elem == arg }
     end
-
     false
   end
 
   def my_none?(arg = nil)
     if arg.nil?
       my_each { |elem| return false if yield(elem) } if block_given?
-      my_each { |elem| return false unless elem.nil? || elem == false }
-      true
-
-    elsif arg.is_a?(Class)
-      my_each { |elem| return false if elem.is_a?(arg) }
-      true
-    elsif arg.is_a?(Regexp)
-      my_each { |elem| return false if elem.match?(arg) }
-      true
+      my_each { |elem| return false unless elem.nil? || elem == false } unless block_given?
+    else
+      my_each { |elem| return false if elem.is_a?(arg) } if arg.is_a?(Class)
+      my_each { |elem| return false if elem.match?(arg) } if arg.is_a?(Regexp)
+      my_each { |elem| return false if elem == arg } if arg
     end
+    true
   end
 
   def my_count(num = nil)
@@ -130,12 +123,4 @@ def multiply_els(array)
   array.my_inject(:*)
 end
 
-# w = [1, true, 99].my_all?
-# puts w
-# animals = ["dog", "door", "rod", "blade"]
-# x = animals.my_all?("dogs")
-# puts x
-useless_array = [2, 4, 2, 2, 4, 4, 2, 7, 1, 4, 4, 3, 5, 4, 1, 2, 7, 7, 4, 8, 4, 2, 1, 6, 2, 6, 6, 7, 8, 7, 6, 6, 1, 4, 7, 7, 2, 3, 6, 2, 7, 0, 7, 5, 0, 4, 6, 5, 4, 5, 4, 0, 0, 2, 7, 8, 3, 4, 0, 0, 2, 4, 3, 6, 5, 8, 4, 8, 4, 4, 3, 5, 5, 5, 4, 6, 6, 7, 1, 4, 7, 3, 4, 8, 8, 6, 7, 4, 8, 8, 5, 2, 0, 4, 2, 1, 6, 2, 7, 2]
-y = useless_array.my_all?(3)
-puts y
 # rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
