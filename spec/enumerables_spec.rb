94f19d42f4566c19fb824b9141d30f1b89f97070
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-07 16:58:13 +0100

    Add tests for my_all

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,8 +1,9 @@
 require_relative "../lib/enumerables"
 
 describe Enumerable do
-    let(:my_array){[*1..4]}
+    let(:my_array){[*1..100]}
     let(:my_hash){{"name" =>"maryjane", "school"=> "microverse"}}
+    
     
     describe "#my_each" do
         it "should return the array when a block is passed" do
@@ -39,7 +40,25 @@ describe Enumerable do
             expect(my_array.my_select).to be_a Enumerable
         end
         it "when a block is given it should return elements satisfy the condition in the block" do
-            expect(my_array.my_select{|num|  num.even? }).to be == ([2, 4])
+            expect(my_array.my_select{|num|  num.even? }).to be == (my_array.select{|num|  num.even? })
         end
-    end   
+    end 
+    describe "#my_all" do
+        it "when block is given" do
+            expect(my_array.my_all?{|x| x > 10}).to eql(my_array.all?{|x| x > 10})
+        end
+        it "when all elements in the array passes the given condition" do
+            expect(%w[ant bear cat].my_all? { |word| word.length >= 3 }).to be == (true)
+        end
+        it "when all elements of the array is odd" do
+            expect(my_array.all?{|num| num.odd? } ).to be == (false)
+        end
+        it "when all elements in the array are integers" do
+            expect(my_array.all?{|num| num.integer? } ).to be == (true)
+        end
+        it "when argument is a regular expression" do
+            expect(%w[ant bear cat].all?(/t/)).to eql(false)
+        end
+        
+    end
 end
